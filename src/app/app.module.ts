9f0b1abdc7e4a7ca8619cbba560e49fd93d0432f
Celina-W <celinawu@hunterschools.org>
2018-03-17 15:06:01 -0400

    Using DataService function to access JSON file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 // importing plugins
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { GoogleMaps } from '@ionic-native/google-maps';
+import { DataServiceProvider } from '../providers/data-service/data-service';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
@@ -25,6 +27,7 @@ import { GoogleMaps } from '@ionic-native/google-maps';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -41,7 +44,8 @@ import { GoogleMaps } from '@ionic-native/google-maps';
     // adding imported plugins
     BarcodeScanner,
     GoogleMaps,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    DataServiceProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,21 +4,44 @@ import { AboutPage } from '../about/about';
 
 // importing plugins
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { DataServiceProvider } from '../../providers/data-service/data-service';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  products: any;
+  selectedProduct: any;
+  productFound:boolean = false;
+
   // construct page with imported items
-  constructor(public navCtrl: NavController, public modalCtrl: ModalController, private barcodeScanner: BarcodeScanner) {}
+  constructor(
+    public navCtrl: NavController,
+    public modalCtrl: ModalController,
+    private barcodeScanner: BarcodeScanner,
+    public dataService: DataServiceProvider
+   ) {
+    this.dataService.getListDetails()
+    .subscribe((response)=> {
+      this.products = response
+      console.log(this.products);
+     });
+  }
 
   // barcode plugin code
   scanBarcode() {
+    this.selectedProduct = {};
     this.barcodeScanner.scan().then(
     // print barcode data to console
     (barCodeData) => {
-      console.log(barCodeData);
+      this.selectedProduct = this.products.find(product => product.plu === barCodeData.text);
+      if(this.selectedProduct !== undefined) {
+      this.productFound = true;
+      } else {
+      this.productFound = false;
+      console.log("barcode data not found");
+      }
     },
     // print error details to console
     (err) => {
@@ -28,4 +51,3 @@ export class HomePage {
     );
   }
 }
-
